c41b7cd7e9cbce595337401282b533a313af8c30
Shanthanu <shanthanu.s.rai9@gmail.com>
2019-12-09 22:19:40 +0530

    nest: Update class structure in topology.py
    Add Namespace class from which Node and Router classes
    are derived.
    
    Signed-off-by: Shanthanu <shanthanu.s.rai9@gmail.com>

--- nest/topology.py
+++ nest/topology.py
@@ -2,26 +2,29 @@
 # Copyright (c) 2019 NITK Surathkal
 
 # Define network topology creation helpers
+from address import Address
 
-import Address
-
-# Assume gen_unique_id function
-
-class Node:
+class Namespace:
 
     def __init__(self):
-        self.id = gen_unique_id()
+        # Assign unique iproute2 name
+        self.id = id(self)
+        self.interfaces = []
 
+    def add_route(self, dest_addr, nxt_hop_addr, via_int):
+        pass
+
+class Node(Namespace):
+
+    def __init__(self):
+        pass
         # Create namespace in iproute2
-        create_ns(self.id)
 
-    # def add_route(self):
-    
-class Router(Node):
+class Router(Namespace):
 
     def __init__(self):
 
-        Node.__init__(self)
+        Namespace.__init__(self)
 
         # Enable forwarding
         enable_forwarding(Node.id)
