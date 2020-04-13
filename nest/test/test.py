05d41577b305d6f41c98b372417b19db9b0f6162
Shanthanu <shanthanu.s.rai9@gmail.com>
2020-04-13 22:10:39 +0530

    test: Add setter and getters for Flow test.py
    Move input verification of Flow API from __init__ to respective setter
    functions. Setter functions allow the user to edit properties of the
    Flow object.
    
    Signed-off-by: Shanthanu <shanthanu.s.rai9@gmail.com>

--- nest/test/test.py
+++ nest/test/test.py
@@ -14,7 +14,8 @@ class Flow():
     Defines a flow in the topology
     """
 
-    def __init__(self, source_node, destination_node, destination_address, start_time, stop_time, number_of_flows, cong_alg='cubic'):
+    def __init__(self, source_node, destination_node, destination_address, start_time, stop_time, 
+            number_of_flows, cong_alg='cubic'):
         """
         'Flow' object in the topology
 
@@ -34,24 +35,140 @@ class Flow():
         :type cong_alg: string
         """
 
-        # Verify types of all args
+        self.set_source_node(source_node)
+        self.set_destination_node(destination_node)
+        self.set_destination_address(destination_address)
+        self.set_start_time(start_time)
+        self.set_stop_time(stop_time)
+        self.set_number_of_flows(number_of_flows)
+        self.set_cong_alg(cong_alg)
+
+    def set_source_node(self, source_node):
+        """
+        Setter for source node
+
+        :param source_node: Source node of flow
+        :type source_node: Node
+        """
+         
         error_handling.type_verify('source_node', source_node, 'Namespace', [Node, Router])
+        self.source_node = source_node
+
+    def set_destination_node(self, destination_node):
+        """
+        Setter for destination node of flow
+
+        :param destination_node: Destination node of flow
+        :type destination_node: Node
+        """
+
         error_handling.type_verify('destination_node', destination_node, 'Namespace', [Node, Router]) 
-        # Verify address passed by user
+        self.destination_node = destination_node
+    
+    def set_destination_address(self, destination_address):
+        """
+        Setter for destination address of flow
+
+        :param destination_address: Destination address of flow
+        :type destination_address: Address/string
+        """
+
         if type(destination_address) is str:
             destination_address = Address(destination_address)
-        error_handling.type_verify('start_time', start_time, 'int', int)
-        error_handling.type_verify('stop_time', stop_time, 'int', int)
-        error_handling.type_verify('number_of_flows', number_of_flows, 'int', int)
-
-        self.source_node = source_node
-        self.destination_node = destination_node
         self.destination_address = destination_address
-        self.start_time = start_time
-        self.stop_time = stop_time
-        self.number_of_flows = number_of_flows
-        self.cong_alg = cong_alg
 
+    def set_start_time(self, start_time):
+        """
+        Setter for start time of flow
+
+        :param start_time: Time to start flow
+        :type start_time: int
+        """
+
+        error_handling.type_verify('start_time', start_time, 'int', int)
+        self.start_time = start_time
+
+    def set_stop_time(self, stop_time):
+        """
+        Setter for stop time of flow
+
+        :param stop_time: Time to stop flow
+        :type stop_time: int
+        """
+
+        error_handling.type_verify('stop_time', stop_time, 'int', int)
+        self.stop_time = stop_time
+
+    def set_number_of_flows(self, number_of_flows):
+        """
+        Setter for number of flows
+
+        :param number_of_flows: Number of flows
+        :type number_of_flows: int
+        """
+
+        error_handling.type_verify('number_of_flows', number_of_flows, 'int', int)
+        self.number_of_flows = number_of_flows
+
+    def set_cong_alg(self, cong_alg):
+        """
+        Setter for congestion algorithm to be used in flow
+
+        :param cong_alg: congestion algorithm to be used during the flow. Defaults to cubic
+        :type cong_alg: string
+        """
+        
+        # TODO: Verify if cong_algo is of right type
+        self.cong_alg = cong_alg
+     
+    def get_source_node(self):
+        """
+        Getter for source node of flow
+        """
+
+        return self.source_node
+
+    def get_destination_node(self):
+        """
+        Getter for destination node of flow
+        """
+
+        return self.destination_node
+
+    def get_destination_address(self):
+        """
+        Getter for destination address of flow
+        """
+
+        return self.destination_address
+
+    def get_start_time(self):
+        """
+        Getter for start time of flow
+        """
+
+        return self.start_time
+
+    def get_stop_time(self):
+        """
+        Getter for stop time of flow
+        """
+
+        return self.stop_time
+
+    def get_number_of_flows(self):
+        """
+        Getter for number of flows
+        """
+
+        return self.number_of_flows
+
+    def get_cong_alg(self):
+        """
+        Getter for congestion control algorithm used in flow
+        """
+
+        return self.cong_alg
 
     def _get_props(self):
         """
