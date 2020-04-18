1477ba757bf3dfee71ab7379894d59e680a85b8a
Narayan G <gnarayang@gmail.com>
2020-04-18 23:07:11 +0530

    examples: Add optimal code for gfc-1 topology
    Signed-off-by: Narayan G <gnarayang@gmail.com>

--- examples/gfc-1.py
+++ examples/gfc-1.py
@@ -9,193 +9,159 @@ import sys
 sys.path.append('../')
 
 from nest.topology import *
-from nest.experiment import *
+from nest.test import *
 
 #############################################
 # This is an implementation of gfc-1 topology
 # GFC Topology:
-#                      d2      f2      a2    c2      e2
+#                      n9     n11      n6    n8      n10
 #                       \       \       \    /       /
 #                        \       \       \  /       /
 #                         \       \       \/       /
-#         a1------r1------r2------r3------r4------r5------b2
+#         n0------r0------r1------r2------r3------r4------n7
 #                 /       /\       \       \
 #                /       /  \       \       \
 #               /       /    \       \       \
-#              d1      b1    f1      c1      e1
+#              n3      n1    n5      n2      n4
 #############################################
 
-a1 = Node('a1')
-b1 = Node('b1')
-c1 = Node('c1')
-d1 = Node('d1')
-e1 = Node('e1')
-f1 = Node('f1')
+r0_r1_bandwidth = input('Enter the bandwidth between r0 and r1\n')
+r1_r2_bandwidth = input('Enter the bandwidth between r1 and r2\n')
+r2_r3_bandwidth = input('Enter the bandwidth between r2 and r3\n')
+r3_r4_bandwidth = input('Enter the bandwidth between r3 and r4\n')
 
-r1 = Node('r1')
-r2 = Node('r2')
-r3 = Node('r3')
-r4 = Node('r4')
-r5 = Node('r5')
+link_bandwidth = input('Enter the bandwidth between the routers and the nodes\n')
 
-a2 = Node('a2')
-b2 = Node('b2')
-c2 = Node('c2')
-d2 = Node('d2')
-e2 = Node('e2')
-f2 = Node('f2')
+r0_r1_latency = input('Enter the latency between r0 and r1\n')
+r1_r2_latency = input('Enter the latency between r1 and r2\n')
+r2_r3_latency = input('Enter the latency between r2 and r3\n')
+r3_r4_latency = input('Enter the latency between r3 and r4\n')
 
-(r1_a1, a1_r1) = connect(r1, a1)
-(r2_b1, b1_r2) = connect(r2, b1)
-(r1_d1, d1_r1) = connect(r1, d1)
-(r2_f1, f1_r2) = connect(r2, f1)
-(r3_c1, c1_r3) = connect(r3, c1)
-(r4_e1, e1_r4) = connect(r4, e1)
-(r2_r1, r1_r2) = connect(r2, r1)
-(r3_r2, r2_r3) = connect(r3, r2)
-(r4_r3, r3_r4) = connect(r4, r3)
-(r5_r4, r4_r5) = connect(r5, r4)
-(r4_a2, a2_r4) = connect(r4, a2)
-(r5_b2, b2_r5) = connect(r5, b2)
-(r2_d2, d2_r2) = connect(r2, d2)
-(r3_f2, f2_r3) = connect(r3, f2)
-(r4_c2, c2_r4) = connect(r4, c2)
-(r5_e2, e2_r5) = connect(r5, e2)
+link_latency = input('Enter the latency between the routers and the nodes\n')
 
-a1_r1.set_address('10.0.1.1/24')
-a2_r4.set_address('10.0.6.6/24')
-b1_r2.set_address('10.0.11.1/24')
-b2_r5.set_address('10.0.18.5/24')
-d1_r1.set_address('10.0.9.1/24')
-d2_r2.set_address('10.0.10.1/24')
-f1_r2.set_address('10.0.12.1/24')
-f2_r3.set_address('10.0.13.1/24')
-c1_r3.set_address('10.0.14.1/24')
-c2_r4.set_address('10.0.15.2/24')
-e1_r4.set_address('10.0.16.3/24')
-e2_r5.set_address('10.0.17.5/24')
-r2_d2.set_address('10.0.10.3/24')
-r1_a1.set_address('10.0.1.2/24')
-r1_d1.set_address('10.0.9.2/24')
-r1_r2.set_address('10.0.2.2/24')
-r2_b1.set_address('10.0.11.3/24')
-r2_r1.set_address('10.0.2.3/24')
-r2_r3.set_address('10.0.3.3/24')
-r2_f1.set_address('10.0.12.3/24')
-r3_r2.set_address('10.0.3.4/24')
-r3_r4.set_address('10.0.4.4/24')
-r3_f2.set_address('10.0.13.4/24')
-r3_c1.set_address('10.0.14.4/24')
-r4_a2.set_address('10.0.6.5/24')
-r4_c2.set_address('10.0.15.5/24')
-r4_r3.set_address('10.0.4.5/24')
-r4_r5.set_address('10.0.5.5/24')
-r4_e1.set_address('10.0.16.5/24')
-r5_r4.set_address('10.0.5.6/24')
-r5_b2.set_address('10.0.18.6/24')
-r5_e2.set_address('10.0.17.6/24')
+qdisc = input('Enter the Queueing algorithm to be installed on the routers\n')
 
-a1.add_route('DEFAULT', a1_r1)
-a2.add_route('DEFAULT', a2_r4)
-r5.add_route('DEFAULT', r5_r4)
+# Creating all the nodes
+node = []
+for i in range (12):
+    node.append(Node('node' + str(i)))
 
-r1.add_route('DEFAULT', r1_r2)
-r2.add_route('DEFAULT', r2_r3)
-r4.add_route('DEFAULT', r4_r5)
+# Creating all the routers
+router = []
+for i in range (5):
+    router.append(Router('router' + str(i)))
 
-b1.add_route('DEFAULT', b1_r2)
-b2.add_route('DEFAULT', b2_r5)
-r4.add_route('10.0.18.0/24', r4_r5)
-r3.add_route('10.0.11.0/24', r3_r2)
-r3.add_route('10.0.18.0/24', r3_r4)
-r4.add_route('10.0.11.0/24', r4_r3)
+# Making the necessary connections
+(r0_n0, n0_r0) = connect(router[0], node[0])
+(r1_n1, n1_r1) = connect(router[1], node[1])
+(r0_n3, n3_r0) = connect(router[0], node[3])
+(r1_n5, n5_r1) = connect(router[1], node[5])
+(r2_n2, n2_r2) = connect(router[2], node[2])
+(r3_n4, n4_r3) = connect(router[3], node[4])
+(r1_r0, r0_r1) = connect(router[1], node[0])
+(r2_r1, r1_r2) = connect(router[2], node[1])
+(r3_r2, r2_r3) = connect(router[3], node[2])
+(r4_r3, r3_r4) = connect(router[4], node[3])
+(r3_n6, n6_r3) = connect(router[3], node[6])
+(r4_n7, n7_r4) = connect(router[4], node[7])
+(r1_n9, n9_r1) = connect(router[1], node[9])
+(r2_n11, n11_r2) = connect(router[2], node[11])
+(r3_n8, n8_r3) = connect(router[3], node[8])
+(r4_n10, n10_r4) = connect(router[4], node[10])
 
-d1.add_route('DEFAULT', d1_r1)
-d2.add_route('DEFAULT', d2_r2)
-r2.add_route('10.0.9.0/24', r2_r1)
-r1.add_route('10.0.10.0/24', r1_r2)
+# Setting addresses to the interfaces
+n0_r0.set_address('10.0.1.1/24')
+n6_r3.set_address('10.0.6.6/24')
+n1_r1.set_address('10.0.11.1/24')
+n7_r4.set_address('10.0.18.5/24')
+n3_r0.set_address('10.0.9.1/24')
+n9_r1.set_address('10.0.10.1/24')
+n5_r1.set_address('10.0.12.1/24')
+n11_r2.set_address('10.0.13.1/24')
+n2_r2.set_address('10.0.14.1/24')
+n8_r3.set_address('10.0.15.2/24')
+n4_r3.set_address('10.0.16.3/24')
+n10_r4.set_address('10.0.17.5/24')
+r1_n9.set_address('10.0.10.3/24')
+r0_n0.set_address('10.0.1.2/24')
+r0_n3.set_address('10.0.9.2/24')
+r0_r1.set_address('10.0.2.2/24')
+r1_n1.set_address('10.0.11.3/24')
+r1_r0.set_address('10.0.2.3/24')
+r1_r2.set_address('10.0.3.3/24')
+r1_n5.set_address('10.0.12.3/24')
+r2_r1.set_address('10.0.3.4/24')
+r2_r3.set_address('10.0.4.4/24')
+r2_n11.set_address('10.0.13.4/24')
+r2_n2.set_address('10.0.14.4/24')
+r3_n6.set_address('10.0.6.5/24')
+r3_n8.set_address('10.0.15.5/24')
+r3_r2.set_address('10.0.4.5/24')
+r3_r4.set_address('10.0.5.5/24')
+r3_n4.set_address('10.0.16.5/24')
+r4_r3.set_address('10.0.5.6/24')
+r4_n7.set_address('10.0.18.6/24')
+r4_n10.set_address('10.0.17.6/24')
 
-f1.add_route('DEFAULT', f1_r2)
-f2.add_route('DEFAULT', f2_r3)
-r2.add_route('10.0.13.0/24', r2_r3)
-r3.add_route('10.0.12.0/24', r3_r2)
+# Adding default routes and subnet specific routes to route the network
+node[0].add_route('DEFAULT', n0_r0)
+node[6].add_route('DEFAULT', n6_r3)
+router[4].add_route('DEFAULT', r4_r3)
 
-c1.add_route('DEFAULT', c1_r3)
-c2.add_route('DEFAULT', c2_r4)
-r3.add_route('10.0.15.0/24', r3_r4)
-r4.add_route('10.0.14.0/24', r4_r3)
+router[0].add_route('DEFAULT', r0_r1)
+router[1].add_route('DEFAULT', r1_r2)
+router[3].add_route('DEFAULT', r3_r4)
 
-e1.add_route('DEFAULT', e1_r4)
-e2.add_route('DEFAULT', e2_r5)
-r4.add_route('10.0.17.0/24', r4_r5)
-r5.add_route('10.0.16.0/24', r5_r4)
+node[1].add_route('DEFAULT', n1_r1)
+node[7].add_route('DEFAULT', n7_r4)
+router[3].add_route(r4_n7.get_subnet(), r3_r4)
+router[2].add_route(r1_n1.get_subnet(), r2_r1)
+router[2].add_route(r4_n7.get_subnet(), r2_r3)
+router[3].add_route(r1_n1.get_subnet(), r3_r2)
 
-r2.add_route('10.0.6.0/24', r2_r3)
-r3.add_route('10.0.5.0/24', r3_r4)
-r3.add_route('10.0.6.0/24', r3_r4)
-r4.add_route('10.0.1.0/24', r4_r3)
-r3.add_route('10.0.1.0/24', r3_r2)
-r2.add_route('10.0.2.0/24', r3_r2)
-r2.add_route('10.0.1.0/24', r2_r1)
+node[3].add_route('DEFAULT', n3_r0)
+node[9].add_route('DEFAULT', n9_r1)
+router[1].add_route(r0_n3.get_subnet(), r1_r0)
+router[0].add_route(r1_n9.get_subnet(), r0_r1)
 
-# node_router_bandwidth = input('Enter the link access bandwidth')
-# print(node_router_bandwidth)
+node[5].add_route('DEFAULT', n5_r1)
+node[11].add_route('DEFAULT', n11_r2)
+router[1].add_route(r2_n11.get_subnet(), r1_r2)
+router[2].add_route(r1_n5.get_subnet(), r2_r1)
 
-# r1_r2_bandwidth = input('Enter the bandwidth between router r1 and r2')
+node[2].add_route('DEFAULT', n2_r2)
+node[8].add_route('DEFAULT', n8_r3)
+router[2].add_route(r3_n8.get_subnet(), r2_r3)
+router[3].add_route(r2_n2.get_subnet(), r3_r2)
 
-# r2_r3_bandwidth = input('Enter the bandwidth between router r2 and r3')
+node[4].add_route('DEFAULT', n4_r3)
+node[10].add_route('DEFAULT', n10_r4)
+router[3].add_route(r4_n10.get_subnet(), r3_r4)
+router[4].add_route(r3_n4.get_subnet(), r4_r3)
 
-# r3_r4_bandwidth = input('Enter the bandwidth between router r3 and r4')
+router[1].add_route(r3_n6.get_subnet(), r1_r2)
+router[2].add_route(r3_r4.get_subnet(), r2_r3)
+router[2].add_route(r3_n6.get_subnet(), r2_r3)
+router[3].add_route(r0_n0.get_subnet(), r3_r2)
+router[2].add_route(r0_n0.get_subnet(), r2_r1)
+router[1].add_route(r0_r1.get_subnet(), r2_r1)
+router[1].add_route(r0_n0.get_subnet(), r1_r0)
 
-# r4_r5_bandwidth = input('Enter the bandwidth between router r4 and r5')
+# Setting attributes to the interfaces on the nodes and the opposite end
+for n in node:
+    for i in n.interface_list:
+        i.set_attributes(link_bandwidth, link_latency)
+        i.get_pair().set_attributes(link_bandwidth, link_latency)
 
-# qdisc = input('Enter the Queueing algorithm for the nodes')
+# Setting attributes for the interfaces between routers
+r0_r1.set_attributes(r0_r1_bandwidth, r0_r1_latency, qdisc)
+r1_r0.set_attributes(r0_r1_bandwidth, r0_r1_latency, qdisc)
 
-sample_bandwidth = '50'
-sample_delay = '100'
-sample_qdisc = 'pfifo'
+r1_r2.set_attributes(r1_r2_bandwidth, r1_r2_latency, qdisc)
+r2_r1.set_attributes(r1_r2_bandwidth, r1_r2_latency, qdisc)
 
-a1_r1.set_attributes(sample_bandwidth, sample_delay)
-r1_a1.set_attributes(sample_bandwidth, sample_delay)
+r2_r3.set_attributes(r2_r3_bandwidth, r2_r3_latency, qdisc)
+r3_r2.set_attributes(r2_r3_bandwidth, r2_r3_latency, qdisc)
 
-b1_r2.set_attributes(sample_bandwidth, sample_delay)
-r2_b1.set_attributes(sample_bandwidth, sample_delay)
-
-d1_r1.set_attributes(sample_bandwidth, sample_delay)
-r1_d1.set_attributes(sample_bandwidth, sample_delay)
-
-f1_r2.set_attributes(sample_bandwidth, sample_delay)
-r2_f1.set_attributes(sample_bandwidth, sample_delay)
-
-c1_r3.set_attributes(sample_bandwidth, sample_delay)
-r3_c1.set_attributes(sample_bandwidth, sample_delay)
-
-e1_r4.set_attributes(sample_bandwidth, sample_delay)
-r4_e1.set_attributes(sample_bandwidth, sample_delay)
-
-qdisc_args = {
-    'ce_threshold' : '1.6ms',
-    'limit' : '1000',
-    'target' : '100ms',
-    'ecn' : ''
-}
-r1_r2.set_attributes(sample_bandwidth, sample_delay, 'pfifo', **qdisc_args)
-r2_r1.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r2_r3.set_attributes(sample_bandwidth, sample_delay, 'pfifo', **qdisc_args)
-r3_r2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r3_r4.set_attributes(sample_bandwidth, sample_delay, 'pfifo', **qdisc_args)
-r4_r3.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r4_r5.set_attributes(sample_bandwidth, sample_delay, 'pfifo', **qdisc_args)
-r5_r4.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-e2_r5.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r5_e2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-c2_r4.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r4_c2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-f2_r3.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r3_f2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-d2_r2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r2_d2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-b2_r5.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r5_b2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-a2_r4.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
-r4_a2.set_attributes(sample_bandwidth, sample_delay, 'pfifo')
+r3_r4.set_attributes(r3_r4_bandwidth, r3_r4_latency, qdisc)
+r4_r3.set_attributes(r3_r4_bandwidth, r3_r4_latency, qdisc)
