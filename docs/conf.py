366da8b7dbb0444c8f06a8e5a5d2297ef0a71e96
Shanthanu <shanthanu.s.rai9@gmail.com>
2020-07-12 11:08:59 +0530

    docs: Add sphinx extensions
    Add some structure to the web page.
    
    Signed-off-by: Shanthanu <shanthanu.s.rai9@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,58 +1,31 @@
 # SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) 2019-2020 NITK Surathkal
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Path setup
 
-# -- Path setup --------------------------------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
+# Project information
 
 project = 'NeST'
 copyright = '2020, NITK'
 author = 'Shanthanu S Rai, Narayan G, Dhanasekhar M, Leslie Monis, Mohit P. Tahiliani'
+release = '0.0.1'
 
-# The full version, including alpha/beta/rc tags
-release = '0.1'
+# General configuration
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'doc-venv']
+# Options for HTML output
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
--- docs/tutorial.rst
+++ docs/tutorial.rst
@@ -1,11 +1,8 @@
 .. SPDX-License-Identifier: GPL-2.0-only
    Copyright (c) 2019-2020 NITK Surathkal
 
-Welcome to NeST's documentation!
-================================
-
-What is NeST?
--------------
+Tutorial
+========
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in nulla at ex
 rhoncus pretium. Curabitur ultricies arcu et tellus commodo, iaculis viverra
@@ -18,11 +15,16 @@ lorem. Vestibulum hendrerit augue at lorem dignissim, at viverra ante bibendum.
 Phasellus eget congue enim. Donec finibus pellentesque libero, et accumsan augue
 tempor in. 
 
-Contents
---------
+Peer to peer topology
+---------------------
 
-.. toctree::
-   :maxdepth: 2
-
-   tutorial.rst
-   api.rst
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in nulla at ex
+rhoncus pretium. Curabitur ultricies arcu et tellus commodo, iaculis viverra
+nibh pretium. Donec nisi nibh, feugiat sed ligula ac, bibendum molestie felis.
+Nullam eget massa id tortor luctus sodales id non dui. Cras enim lacus, euismod
+sed laoreet vitae, ornare ut nisl. Integer eget metus felis. Nam sed ipsum at
+urna finibus blandit ac a magna. Interdum et malesuada fames ac ante ipsum
+primis in faucibus. Phasellus ut lectus lacinia, euismod mauris sit amet, cursus
+lorem. Vestibulum hendrerit augue at lorem dignissim, at viverra ante bibendum.
+Phasellus eget congue enim. Donec finibus pellentesque libero, et accumsan augue
+tempor in. 
