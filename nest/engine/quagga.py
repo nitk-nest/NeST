d21f69e95a2b04bd4a8434de000437705dc8defd
Dhanasekhar <dhanasekhar1699@gmail.com>
2020-10-17 16:14:14 +0530

    routing: add base class for quagga
    Signed-off-by: Dhanasekhar <dhanasekhar1699@gmail.com>

---
+++ nest/engine/quagga.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2020 NITK Surathkal
+
+"""quagga commands"""
+
+from .exec import exec_subprocess
+
+def run_quagga(ns_id, daemon, conf_file, pid_file):
+    """
+    Runs quagga related daemons like zebra, ospf and rip
+
+    Parameters
+    ----------
+    ns_id : str
+        namespace of the router
+    daemon : str
+        quagga process to run(one of ['zebra'. 'ospfd])
+    conf_file : str
+        path to config file
+    pid_file : str
+        path to pid file
+    """
+    cmd = f'ip netns exec {ns_id} {daemon} -f {conf_file} -i {pid_file} -d'
+    print(cmd)
+    exec_subprocess(cmd)
+
+def create_quagga_directory(dir_path):
+    """
+    Create a quagga owned directory for quagga configs
+
+    Parameters
+    ----------
+    dir_path : str
+        path to config directory
+    """
+    cmd = f'sudo -u quagga -g quaggavty mkdir {dir_path}'
+    exec_subprocess(cmd)
+
+def create_conf_file(filename):
+    """
+    Create a quagga owned config file
+
+    Parameters
+    ----------
+    filename : str
+        path to config file
+    """
+    cmd = f'sudo -u quagga -g quaggavty touch {filename}'
+    exec_subprocess(cmd)
---
+++ nest/routing/quagga_base.py
@@ -0,0 +1,74 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2020 NITK Surathkal
+
+"""
+Base class for Quagga
+"""
+
+from abc import ABC, abstractmethod
+import io
+import shutil
+from nest.engine.quagga import run_quagga, create_conf_file
+
+
+class QuaggaBase(ABC):
+    """
+    Abstract class for Quagga related processes. This class should
+    be inherited for adding other quagga daemons
+
+    Attributes
+    ----------
+    conf: file-like
+        In memory stream to temporarily hold configuration
+    router_ns_id : str
+        Router namespace id
+    daemon : str
+        quagga daemon to run(one of ['zebra', 'ospf'])
+    conf_file : str
+        config file path
+    pid_file : str
+        pid file path for the daemon process
+    interfaces : List[Interface]
+        interfaces present in the router
+    """
+
+    def __init__(self, router_ns_id, interfaces, daemon, conf_dir):
+        self.conf = io.StringIO()
+        self.router_ns_id = router_ns_id
+        self.daemon = daemon
+        self.conf_file = f'{conf_dir}/{self.router_ns_id}_{daemon}.conf'
+        self.pid_file = f'{conf_dir}/{self.router_ns_id}_{daemon}.pid'
+        self.interfaces = interfaces
+
+    @abstractmethod
+    def create_basic_config(self):
+        """
+        Created minimum configuration for `daemon`
+        """
+
+    def add_to_config(self, command):
+        """
+        Add a line to `self.conf`
+
+        Parmeters
+        ---------
+        command : str
+            command to add to config file
+        """
+        self.conf.write(f'{command}\n')
+
+    def create_config(self):
+        """
+        Creates config file on disk from `self.conf`
+        """
+        print(self.conf_file)
+        create_conf_file(self.conf_file)
+        with open(self.conf_file, 'w') as conf:
+            self.conf.seek(0)
+            shutil.copyfileobj(self.conf, conf)
+
+    def run(self):
+        """
+        Run the `daemon` along with its config file
+        """
+        run_quagga(self.router_ns_id, self.daemon, self.conf_file, self.pid_file)
