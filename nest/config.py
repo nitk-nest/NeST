eeac3f28f8a54b46620bf0890d63a80236086b04
Narayan G <gnarayang@gmail.com>
2020-11-02 12:14:39 +0530

    nest: Add method to read custom config through json
    Signed-off-by: Narayan G <gnarayang@gmail.com>

--- nest/config.py
+++ nest/config.py
@@ -20,7 +20,6 @@ def import_default_config():
     """
     with open(os.path.realpath(os.path.dirname(__file__)) + '/config.json', 'r') as json_file:
         data = json.load(json_file)
-        # print(data)
         return data
 
 def set_value(parameter, value):
@@ -61,11 +60,40 @@ def get_value(parameter):
     """
     if parameter in default_value:
         return default_value[parameter]
-    elif parameter in default_value['topology']:
+    if parameter in default_value['topology']:
         return default_value['topology'][parameter]
-    elif parameter in default_value['experiment']:
+    if parameter in default_value['experiment']:
         return default_value['experiment'][parameter]
-    elif parameter in default_value['engine']:
+    if parameter in default_value['engine']:
         return default_value['engine'][parameter]
-    else:
-        logging.error('The given parameter %s does not exist', parameter)
+    # If it belonged to none of them
+    logging.error('The given parameter %s does not exist', parameter)
+    return 'The given parameter %s does not exist'
+
+def import_custom_config(path):
+    """
+    Overwrites the current value of config parameters with the values
+    from the JSON in the given path
+
+    Attributes
+    ----------
+    path: str
+        The path in which the json file
+    """
+    with open(path, 'r') as json_file:
+        data = json.load(json_file)
+    if 'topology' in data:
+        for parameter in data['topology']:
+            if parameter in default_value['topology']:
+                default_value['topology'][parameter] = data['topology'][parameter]
+    elif 'experiment' in data:
+        for parameter in data['experiment']:
+            if parameter in default_value['experiment']:
+                default_value['experiment'][parameter] = data['experiment'][parameter]
+    elif 'engine' in data:
+        for parameter in data['engine']:
+            if parameter in default_value['engine']:
+                default_value['engine'][parameter] = data['engine'][parameter]
+    for parameter in data:
+        if not isinstance(default_value[parameter], dict):
+            default_value[parameter] = data[parameter]
