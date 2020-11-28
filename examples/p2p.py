27d8bb1e7aa986a67fafbfad04d4456e29964e88
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2020-11-28 19:43:03 +0530

    examples/p2p.py: Improve documentation
    The documentation had some typos and was confusing at some places. It
    has been updated now to be more understandable.
    
    Signed-off-by: Shanthanu S Rai <shanthanu.s.rai9@gmail.com>

--- examples/p2p.py
+++ examples/p2p.py
@@ -14,17 +14,19 @@ from nest.topology import *
 ##############################
 
 # Creates two nodes, with the names n0 and n1
-# and returns it to the python variables `n0` and `n1`
-# The API takes in the name of the node as a string
-# the same name is used to return results
+# and returns it to the variables `n0` and `n1`
+# The API takes in the name of the node as a string.
+# This name is used while displaying results.
 n0 = Node('n0')
 n1 = Node('n1')
 
-# Connects the two namespaces using a veth pair
-# and returns the interfaces as a touple to
-# `n0_n1` and `n1_n0`
+# Connects the above two nodes using a veth (virtual ethernet)
+# pair and returns the interfaces at the end points of the link
+# as a tuple `n0_n1` and `n1_n0`. `n0_n1` interface
+# is at `n0` and `n1_n0` interface is at `n1`.
+#
 # The API takes two nodes as the parameters
-# and the order does not matter
+# and returns the pair of interfaces
 (n0_n1, n1_n0) = connect(n0, n1)
 
 # Sets address to both the interfaces
@@ -34,25 +36,30 @@ n0_n1.set_address('10.0.0.1/24')
 n1_n0.set_address('10.0.0.2/24')
 
 # Sets attributes such as bandwidth, latency and
-# queue descipline to the connectivity
-# Note that the latency and bandwidth need not be the same,
-# as in the real life scenario where upload bandwidth is
-# lesser than download
+# queue discipline for the link.
+# Attributes for the link from `n0` to `n1` are set at interface
+# `n0_n1` (and vice versa)
+# Note that the bandwidth (and latency) need not be the same in both
+# diretions, as in the real life scenario where upload bandwidth is
+# typically lower than download bandwidth
 n0_n1.set_attributes('5mbit', '5ms', 'pfifo')
 n1_n0.set_attributes('10mbit', '100ms', 'pfifo')
 
-# Sets up a flow between the two nodes
+# Defines a flow between the two nodes `n0` and `n1`
 # The API takes in the source node, destination node,
-# destination address, start time, end time of the
-# experiment to be run and the number of flows
+# destination address, start time and end time of
+# the flow and the number of flows
 flow = Flow(n0, n1, n1_n0.address, 0, 10, 2)
 
-# The name of the experiment of the is taken to
-# return the results according with the name
-# the flavour of tcp to be used is also given
-exp = Experiment('tcp_1up')
+# Define an experiment to be run on the above topology.
+# The API takes the experiment name as a string
+exp = Experiment('tcp_2up')
+
+# Add the above defined flow to the experiment.
+# The TCP flavor of the traffic generated can be
+# optionally given. Below we have chosen TCP Reno
 exp.add_tcp_flow(flow, 'reno')
 
-# The experiment is run with the topology and
-# mentioned configurations
+# The experiment is run on the above topology with
+# the mentioned configurations.
 exp.run()
