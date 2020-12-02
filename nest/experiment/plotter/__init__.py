cedd0955f682c7b7cd0c50470cc8574cf6f97723
Shashank D <shashankindiamanoj@gmail.com>
2020-12-02 15:27:36 +0000

    plotter: use agg backend
    Plotting workers were trying to connect to X-server simultaneously. Agg backend is specifically for writing (plots into) files.
    
    Fixes #73.

--- nest/experiment/plotter/__init__.py
+++ nest/experiment/plotter/__init__.py
@@ -2,8 +2,14 @@
 # Copyright (c) 2019-2020 NITK Surathkal
 
 """Handle plotting results obtained"""
+import matplotlib
+from matplotlib import style
 
-import matplotlib.style as style
+# Since plots are not shown in window, use the non-interactive Agg backend.
+# Also fixes https://gitlab.com/nitk-nest/nest/-/issues/73
+# Many plotting processes were connecting to X-server simultaneously.
+# This created errors as X-server couldn't handle requests
+matplotlib.use('agg')
 
 # Set plot style
 style.use('seaborn-paper')
