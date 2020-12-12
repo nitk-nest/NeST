ca8d305f4af9e9b183fff8cbff9527791ff20759
Narayan G <gnarayang@gmail.com>
2020-12-12 21:39:01 +0530

    examples: Standardise p2p.py
    Signed-off-by: Narayan G <gnarayang@gmail.com>

--- examples/p2p.py
+++ examples/p2p.py
@@ -10,46 +10,46 @@ from nest.topology import *
 ##############################
 # Topology
 #
-# n0 ----- n1
+# n1 ----- n2
+#
 ##############################
 
-# Creates two nodes, with the names n0 and n1
-# and returns it to the variables `n0` and `n1`
-# The API takes in the name of the node as a string.
-# This name is used while displaying results.
-n0 = Node('n0')
+# Creates two nodes, with the names n1 and n2
+# and returns it to the python variables `n1` and `n2`
+# The API takes in the name of the node as a string
+# the same name is used to return results
 n1 = Node('n1')
+n2 = Node('n2')
 
 # Connects the above two nodes using a veth (virtual Ethernet)
 # pair and returns the interfaces at the end points of the link
-# as a tuple `n0_n1` and `n1_n0`. `n0_n1` interface
-# is at `n0` and `n1_n0` interface is at `n1`.
-#
+# as a tuple `n1_n2` and `n2_n1`. `n1_n2` interface
+# is at `n1` and `n2_n1` interface is at `n2`.
 # The API takes two nodes as the parameters
 # and returns the pair of interfaces
-(n0_n1, n1_n0) = connect(n0, n1)
+(n1_n2, n2_n1) = connect(n1, n2)
 
 # Sets address to both the interfaces
 # The API takes the address as a string
 # The subnet also needs to be mentioned
-n0_n1.set_address('10.0.0.1/24')
-n1_n0.set_address('10.0.0.2/24')
+n1_n2.set_address('10.0.0.1/24')
+n2_n1.set_address('10.0.0.2/24')
 
 # Sets attributes such as bandwidth, latency and
 # queue discipline for the link.
-# Attributes for the link from `n0` to `n1` are set at interface
-# `n0_n1` (and vice versa)
+# Attributes for the link from `n1` to `n2` are set at interface
+# `n1_n2` (and vice versa)
 # Note that the bandwidth (and latency) need not be the same in both
 # directions, as in the real life scenario where upload bandwidth is
 # typically lower than download bandwidth
-n0_n1.set_attributes('5mbit', '5ms', 'pfifo')
-n1_n0.set_attributes('10mbit', '100ms', 'pfifo')
+n1_n2.set_attributes('5mbit', '5ms', 'pfifo')
+n2_n1.set_attributes('10mbit', '100ms', 'pfifo')
 
-# Defines a flow between the two nodes `n0` and `n1`
+# Defines a flow between the two nodes `n1` and `n2`
 # The API takes in the source node, destination node,
 # destination address, start time and end time of
 # the flow and the number of flows
-flow = Flow(n0, n1, n1_n0.address, 0, 10, 2)
+flow = Flow(n1, n2, n2_n1.address, 0, 10, 2)
 
 # Define an experiment to be run on the above topology.
 # The API takes the experiment name as a string
