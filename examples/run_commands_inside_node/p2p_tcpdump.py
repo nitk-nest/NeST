ad1afe6535a8aee999993f1b2a155f5d594bfcee
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2021-01-02 19:04:10 +0530

    examples: Show running commands inside Node
    Run ping on one of the nodes and capture packets in another node.
    This demonstrates the new API for running commands inside a node.
    
    Signed-off-by: Shanthanu S Rai <shanthanu.s.rai9@gmail.com>

---
+++ examples/run_commands_inside_node/p2p_tcpdump.py
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2021 NITK Surathkal
+
+########################
+# SHOULD BE RUN AS ROOT
+########################
+import subprocess
+import multiprocessing
+
+from nest.experiment import *
+from nest.topology import *
+
+##############################
+# Topology
+#
+# n1 ----- n2
+#
+##############################
+
+# Create two nodes, connect them and assign address to their
+# respctive interfaces
+n1 = Node('n1')
+n2 = Node('n2')
+
+(n1_n2, n2_n1) = connect(n1, n2)
+
+n1_n2.set_address('10.0.0.1/24')
+n2_n1.set_address('10.0.0.2/24')
+
+# Set duration ping run for and number of packets to be captured
+# by tcpdump
+DURATION = 10
+MAX_PACKETS_CAPTURED = 20
+
+# Initiate ping (for 10s) in n1 as a seperate process
+print(f'Running ping for {DURATION} seconds from n1 to n2...')
+process = multiprocessing.Process(target=n1.ping,
+            args=(n2_n1.address, DURATION, False))
+process.start()
+
+# Now run a packet capture in n2
+with n2:
+    print('Running tcpdump in n2 to capture ping packets...')
+    proc = subprocess.Popen(['tcpdump', '-i',  n2_n1.id, f'-c {MAX_PACKETS_CAPTURED}'],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, _) = proc.communicate()
+
+# Output packet capture after ping thread finishes
+process.join()
+print(f'\nPackets captured at n2 by tcpdump (max: {MAX_PACKETS_CAPTURED} packets):\n')
+print(stdout.decode())
