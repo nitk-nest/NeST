682b1d8269995c4a1b53e09cecf56c3008408388
Narayan G <gnarayang@gmail.com>
2021-03-22 17:33:38 +0530

    config: Rearrange if statements alphabetically
    Improves readability of the file and is according to the json file
    
    Signed-off-by: Narayan G <gnarayang@gmail.com>

--- nest/config.py
+++ nest/config.py
@@ -39,16 +39,16 @@ def set_value(parameter, value):
     value: str
         The value to which the parameter has to be changed to
     """
-    if parameter in default_value:
-        default_value[parameter] = value
-    elif parameter in default_value["topology"]:
-        default_value["topology"][parameter] = value
+    if parameter in default_value["engine"]:
+        default_value["engine"][parameter] = value
     elif parameter in default_value["experiment"]:
         default_value["experiment"][parameter] = value
-    elif parameter in default_value["engine"]:
-        default_value["engine"][parameter] = value
     elif parameter in default_value["routing"]:
         default_value["routing"][parameter] = value
+    elif parameter in default_value["topology"]:
+        default_value["topology"][parameter] = value
+    elif parameter in default_value:
+        default_value[parameter] = value
     else:
         logging.error("The given parameter %s does not exist", parameter)
 
@@ -67,16 +67,16 @@ def get_value(parameter):
     str
         The value of the parameter
     """
-    if parameter in default_value:
-        return default_value[parameter]
-    if parameter in default_value["topology"]:
-        return default_value["topology"][parameter]
-    if parameter in default_value["experiment"]:
-        return default_value["experiment"][parameter]
     if parameter in default_value["engine"]:
         return default_value["engine"][parameter]
+    if parameter in default_value["experiment"]:
+        return default_value["experiment"][parameter]
     if parameter in default_value["routing"]:
         return default_value["routing"][parameter]
+    if parameter in default_value["topology"]:
+        return default_value["topology"][parameter]
+    if parameter in default_value:
+        return default_value[parameter]
 
     # If it belonged to none of them
     logging.error("The given parameter %s does not exist", parameter)
@@ -96,22 +96,22 @@ def import_custom_config(path):
     # pylint: disable=too-many-branches
     with open(path, "r") as json_file:
         data = json.load(json_file)
-    if "topology" in data:
-        for parameter in data["topology"]:
-            if parameter in default_value["topology"]:
-                default_value["topology"][parameter] = data["topology"][parameter]
-    if "experiment" in data:
-        for parameter in data["experiment"]:
-            if parameter in default_value["experiment"]:
-                default_value["experiment"][parameter] = data["experiment"][parameter]
     if "engine" in data:
         for parameter in data["engine"]:
             if parameter in default_value["engine"]:
                 default_value["engine"][parameter] = data["engine"][parameter]
+    if "experiment" in data:
+        for parameter in data["experiment"]:
+            if parameter in default_value["experiment"]:
+                default_value["experiment"][parameter] = data["experiment"][parameter]
     if "routing" in data:
         for parameter in data["routing"]:
             if parameter in default_value["routing"]:
                 default_value["routing"][parameter] = data["routing"][parameter]
+    if "topology" in data:
+        for parameter in data["topology"]:
+            if parameter in default_value["topology"]:
+                default_value["topology"][parameter] = data["topology"][parameter]
     for parameter in data:
         if not isinstance(default_value[parameter], dict):
             default_value[parameter] = data[parameter]
