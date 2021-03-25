c504cd2c069737112191df28b83d523c10c5e9ce
Shanthanu S Rai <shanthanu.s.rai9@gmail.com>
2021-03-25 23:46:44 +0530

    tests: Add UDP test for testing iperf3
    Signed-off-by: Shanthanu S Rai <shanthanu.s.rai9@gmail.com>

--- nest/tests/test_experiment.py
+++ nest/tests/test_experiment.py
@@ -42,6 +42,35 @@ class TestExperiment(unittest.TestCase):
 
         exp.run()
 
+    def test_experiment_udp(self):
+        n0 = Node("n0")
+        n1 = Node("n1")
+        r = Node("r")
+        r.enable_ip_forwarding()
+
+        (n0_r, r_n0) = connect(n0, r)
+        (r_n1, n1_r) = connect(r, n1)
+
+        n0_r.set_address("10.1.1.1/24")
+        r_n0.set_address("10.1.1.2/24")
+        r_n1.set_address("10.1.2.2/24")
+        n1_r.set_address("10.1.2.1/24")
+
+        n0.add_route("DEFAULT", n0_r)
+        n1.add_route("DEFAULT", n1_r)
+
+        n0_r.set_attributes("100mbit", "5ms")
+        r_n0.set_attributes("100mbit", "5ms")
+
+        r_n1.set_attributes("10mbit", "40ms", "pie")
+        n1_r.set_attributes("10mbit", "40ms")
+
+        exp = Experiment("test-experiment-udp")
+        flow = Flow(n0, n1, n1_r.address, 0, 5, 2)
+        exp.add_udp_flow(flow)
+
+        exp.run()
+
     def test_experiment_ipv6(self):
         # Test IPv6 with Duplicate Address Detection (DAD) disabled
         n0 = Node("n0")
