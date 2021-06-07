12048b06373a942136c8c31f12b5b5ff89c3a3e9
18divya06 <18divya06@gmail.com>
2021-06-07 23:46:13 +0530

    topology: Add support for IPv4-mapped IPv6 address
    Address class now handles Ipv4 mapped IPv6 address.
    Also handled IPv6 address without prefix by using
    128 as default.
    
    Signed-off-by: Ayushi Jain <ajain3765@gmail.com>
    Signed-off-by: Divya Meena <18divya06@gmail.com>
    Signed-off-by: Himanshu Kashyap <himanshukashyap1122@gmail.com>
    Signed-off-by: Pranab Nandy <nandypranab3@gmail.com>
    Signed-off-by: Satyam Prakash <satyamprakash2828@gmail.com>
    Signed-off-by: Swati Singh <swatikdma@gmail.com>

--- nest/topology/address.py
+++ nest/topology/address.py
@@ -1,16 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2019-2020 NITK Surathkal
+# Copyright (c) 2019-2021 NITK Surathkal
 
 """Handles addresses of interfaces"""
 
 import ipaddress
-from ipaddress import ip_address, IPv6Address
+from ipaddress import ip_address, IPv6Address, IPv4Address
 
 
 class Address:
-    """Validate address.
+    """
+    Validate IP addresses and provide some basic APIs
+    to obtain information about the IP address.
 
-    TODO: Untested for IPv6
     """
 
     def __init__(self, addr_str):
@@ -23,21 +24,31 @@ class Address:
             The string representing the address
         """
 
-        if addr_str.isupper():  # A special string in place of address
-            if addr_str == "DEFAULT":
-                self.ip_addr = addr_str.lower()
-            else:
-                raise ValueError(
-                    addr_str + " is not a special IP string. Perhaps you meant DEFAULT?"
-                )
-        elif "/" in addr_str:  # An interface address
-            # raises exception in invalid case
-            ipaddress.ip_interface(addr_str)
-            self.ip_addr = addr_str
-        else:  # An IP address
-            # raises exception in invalid case
-            ipaddress.ip_address(addr_str)
-            self.ip_addr = addr_str + "/32"
+        # Check if a "special" string is passed
+        if addr_str == "DEFAULT":  # A special string in place of address
+            self.ip_addr = addr_str.lower()
+            return
+
+        # Split address and prefix_len
+        _addr_str, _prefix_len = self._split_address_and_prefix_length(addr_str)
+
+        # if IPv4 address
+        # expected format from user => (10.0.0.0/24 or 10.0.0.0)
+        if self._is_ipv4(_addr_str):
+            self.ip_addr = self._get_ipv4_address(_addr_str, _prefix_len)
+
+        # if IPv4-Mapped IPv6 address
+        # expected format from user => (::FFFF:10.0.0.0/122 or ::FFFF:10.0.0.0)
+        elif self._is_ipv4_mapped_ipv6(_addr_str):
+            self.ip_addr = self._get_ipv4_mapped_ipv6_address(_addr_str, _prefix_len)
+
+        # if IPv6 address
+        # expected format from user => (2a0:101::/122 or 2a0:101::)
+        elif self._is_ipv6(_addr_str):
+            self.ip_addr = self._get_ipv6_address(_addr_str, _prefix_len)
+
+        else:
+            raise ValueError(addr_str + " is not a valid IP address")
 
     def get_addr(self, with_subnet=True):
         """Getter for ip_addr
@@ -75,7 +86,6 @@ class Address:
     def is_ipv6(self):
         """
         Check if the address is IPv6 or not
-
         Note: This takes care of 'DEFAULT' also
         """
         check_addr = self.ip_addr.split("/")[0]
@@ -83,15 +93,151 @@ class Address:
             return True
         return False
 
+    def _split_address_and_prefix_length(self, addr_str):
+        """
+        Split address and prefix length
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+
+        Returns
+        -------
+        tuple
+            addr_str :str
+            prefix_len : int
+
+        """
+        # raises exception in invalid case
+        ipaddress.ip_interface(addr_str)
+
+        if "/" in addr_str:
+            addr_str = addr_str.split("/")
+            prefix_len = int(addr_str[1])
+            addr_str = addr_str[0]
+        else:
+            prefix_len = None
+
+        return addr_str, prefix_len
+
+    def _is_ipv4(self, addr_str):
+        """
+        Check if the address without prefix length is IPv4 or not
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+
+        """
+        return isinstance(ip_address(addr_str), IPv4Address)
+
+    def _is_ipv4_mapped_ipv6(self, addr_str):
+        """
+        Check if the address without prefix length is IPv4-mapped IPv6 or not
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+
+        """
+        return ipaddress.IPv6Address(addr_str).ipv4_mapped is not None
+
+    def _is_ipv6(self, addr_str):
+        """
+        Check if the address without prefix length is IPv6 or not
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+
+        """
+        return isinstance(ip_address(addr_str), IPv6Address)
+
+    def _get_ipv4_address(self, addr_str, prefix_len):
+        """
+        Getter for IPv4 Address
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+        prefix_len : int
+            The prefix length
+
+        Returns
+        -------
+        str
+            IPv4 address as string
+
+        """
+        if prefix_len is None:
+            prefix_len = 32
+        addr_str = addr_str + "/" + str(prefix_len)
+        return addr_str
+
+    def _get_ipv4_mapped_ipv6_address(self, addr_str, prefix_len):
+        """
+        Getter for IPV4-mapped IPv6 Address
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+        prefix_len : int
+            The prefix length
+
+        Returns
+        -------
+        str
+            IPv4 address as string
+
+        """
+        addr_str = str(ipaddress.IPv6Address(addr_str).ipv4_mapped)
+        # If the prefix length is empty or not valid (between 96 and 128), then it is set to 32.
+        # Ipv4-mapped address has 128 bits. The IPv4 address takes the last 32 bits,
+        # and therefore the first 96 (128-32) bits from the address are removed. Therefore
+        # valid prefix length will be in between 96 and 128.
+        if prefix_len is not None and prefix_len in range(97, 128):
+            prefix_len = prefix_len - 96
+        else:
+            prefix_len = None
+        return self._get_ipv4_address(addr_str, prefix_len)
+
+    def _get_ipv6_address(self, addr_str, prefix_len):
+        """
+        Getter for IPv6 Address
+
+        Parameters
+        ----------
+        addr_str : str
+            The string representing the address
+        prefix_len : int
+            The prefix length
+
+        Returns
+        -------
+        str
+            IPv6 address as string
+
+        """
+        if prefix_len is None:
+            prefix_len = "128"
+        addr_str = addr_str + "/" + str(prefix_len)
+        return addr_str
+
     def __repr__(self):
         classname = self.__class__.__name__
         return f"{classname}({self.ip_addr!r})"
 
 
 class Subnet:
-    """For generating sequential addresses in a subnet
+    """
+    For generating sequential addresses in a subnet
 
-    NOTE: Supported only for IPv4 addresses
     """
 
     def __init__(self, addr_str):
