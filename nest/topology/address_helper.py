304f6bc95f167e7b90bcaeb3d174ca3d4e73e40f
ajain3765 <ajain3765@gmail.com>
2021-06-07 23:56:43 +0530

    topology: Add Address Helper class
    Address Helper class is used to support automatic
    address assignment for both IPv4 and IPv6.
    
    Signed-off-by: Ayushi Jain <ajain3765@gmail.com>
    Signed-off-by: Divya Meena <18divya06@gmail.com>
    Signed-off-by: Himanshu Kashyap <himanshukashyap1122@gmail.com>
    Signed-off-by: Pranab Nandy <nandypranab3@gmail.com>
    Signed-off-by: Satyam Prakash <satyamprakash2828@gmail.com>
    Signed-off-by: Swati Singh <swatikdma@gmail.com>

---
+++ nest/topology/address_helper.py
@@ -0,0 +1,60 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2021 NITK Surathkal
+
+"""API related to Address Helper"""
+
+import logging
+from nest.topology_map import TopologyMap
+from .address import Subnet
+
+# pylint: disable=too-few-public-methods
+
+logger = logging.getLogger(__name__)
+
+
+class AddressHelper:
+    """
+    Abstraction for automatic address assignment to interfaces of the network.
+    """
+
+    @staticmethod
+    def assign_addresses(network=None):
+        """
+        Assignment of addresses to the interfaces.
+
+        Parameters
+        ----------
+        network : Network
+            Assigning the addresses to each interfaces of all network.
+            It will assign addresses to specific network if network object is mentioned.
+        """
+        if network is None:
+            for net in TopologyMap.list_of_network:
+                AddressHelper.__assign_addresses_to_network(net)
+
+            # Warning for the assignment of the addresses to the interfaces,
+            # which do not belong to any network
+            if TopologyMap.orphan_interfaces != 0:
+                logger.warning(
+                    "Interfaces not part of some network. They should be manually added",
+                )
+        else:
+            AddressHelper.__assign_addresses_to_network(network)
+
+    @staticmethod
+    def __assign_addresses_to_network(network):
+        """
+        Assignment of addresses to all interfaces of specific network.
+
+        Parameters
+        ----------
+        network : Network
+            Assigning the addresses to each interfaces of the given network object.
+        """
+        _interface = []
+        _interface = network.interface
+
+        _net_address = Subnet(network.net_address)
+
+        for inter in _interface:
+            inter.set_address(_net_address.get_next_addr())
