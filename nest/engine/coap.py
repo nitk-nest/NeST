4aef4db1d4989ccaae5c421298230cfe3ac01f83
Suhas K S <suhasks123@gmail.com>
2021-11-14 18:00:10 +0530

    engine: Implement the CoAP server mechanism
     - engine/coap.py: Add function `run_coap_server()` which starts
       the CoAP server by running `coap_server.py` using
       `exec_subprocess_in_background()` in `engine/exec.py`.
     - engine/coap_server.py: Add implementation for a basic CoAP
       server, with a sample resource whose content value can be set
       with a command line argument.
    
    Signed-off-by: Suhas K S <suhasks123@gmail.com>

--- nest/engine/coap.py
+++ nest/engine/coap.py
@@ -5,7 +5,7 @@
 
 import os
 
-from .exec import exec_exp_commands
+from .exec import exec_exp_commands, exec_subprocess_in_background
 
 # pylint: disable=too-many-arguments
 def run_coap_client(ns_name, destination_ip, ipv6, coap_options, out, err):
@@ -49,4 +49,32 @@ def run_coap_client(ns_name, destination_ip, ipv6, coap_options, out, err):
         f"ip netns exec {ns_name} python3 {path_to_client} -d {destination_ip} {coap_options}",
         stdout=out,
         stderr=err,
+    )
+
+
+def run_coap_server(ns_name, server_options):
+    """
+    Run CoAP server program
+
+    Parameters
+    ----------
+    ns_name : str
+        Name of the server namespace
+    server_options : str
+        Options string for running the server process
+
+    Returns
+    -------
+    int
+        return code of the command executed
+    """
+    # Here, the path to the current file being run `coap.py` is taken
+    # and used to run `coap_server.py` in the same directory. This is
+    # done using the `__file__` dunder variable and the `os` library.
+    path_to_curr_module = os.path.dirname(os.path.abspath(__file__))
+    path_to_server = path_to_curr_module + "/coap_server.py"
+
+    return exec_subprocess_in_background(
+        f"ip netns exec {ns_name} python3 -u {path_to_server} {server_options}",
+        shell=True,
     )
---
+++ nest/engine/coap_server.py
@@ -0,0 +1,133 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2022 NITK Surathkal
+
+""" Sets up a CoAP server with a sample resource """
+
+# Below implementation is based on the examples from:
+# https://aiocoap.readthedocs.io/en/latest/examples.html
+
+import argparse
+import asyncio
+
+import aiocoap.resource as resource
+import aiocoap
+
+
+class SampleResource(resource.Resource):
+    """
+    Represents the resource stored on the server.
+    Supports GET and PUT requests.
+
+    Attributes
+    ----------
+    content : bytes
+        represents the content stored on the server as bytes
+    """
+
+    def __init__(self, content: str):
+        """
+        Constructor to initialize the resource.
+
+        Parameters
+        ----------
+        content : str
+            content to initialize the resource with
+        """
+        # pylint: disable=non-parent-init-called
+        super().__init__()
+        self.set_content(content.encode("utf-8"))
+
+    def set_content(self, content: bytes):
+        """
+        Set content for the resource. Encoded into
+        bytes before setting.
+
+        Parameters
+        ----------
+        content : bytes
+            content to set for the resource
+        """
+        self.content = content
+
+    # pylint: disable=unused-argument
+    async def render_get(self, request):
+        """
+        Handler for GET requests received for the resource
+
+        Parameters
+        ----------
+        request : aiocoap.Message
+            GET request received from the client
+
+        Returns
+        -------
+        aiocoap.Message
+            response message for the GET request
+        """
+        return aiocoap.Message(payload=self.content)
+
+    async def render_put(self, request):
+        """
+        Handler for PUT requests received for the resource
+
+        Parameters
+        ----------
+        request : aiocoap.Message
+            PUT request received from the client
+
+        Returns
+        -------
+        aiocoap.Message
+            response message for the PUT request
+        """
+        self.set_content(request.payload)
+        # pylint: disable=no-member
+        return aiocoap.Message(code=aiocoap.CHANGED, payload=self.content)
+
+
+def main():
+    """
+    Parses the content for the server resource, sets up the
+    root element, adds the resource to the root element, creates
+    the server context and then runs the server process.
+    """
+
+    help_description = "CoAP server"
+
+    # Initialize parser
+    parser = argparse.ArgumentParser(description=help_description)
+
+    # Adding arguments for client request
+    parser.add_argument("-c", "--content", type=str)
+
+    # Read Arguments
+    args = parser.parse_args()
+
+    # Resource tree creation, starting with the root element
+    root_element = resource.Site()
+
+    # For defining the entry point for requesting the list of links
+    # for resources stored in the server. This is for performing
+    # CoRE(Constrained RESTful Environments) Resource Discovery.
+    root_element.add_resource(
+        [".well-known", "core"],
+        resource.WKCResource(root_element.get_resources_as_linkheader),
+    )
+
+    # Adding the SampleResource defined in this file
+    root_element.add_resource(["sample-resource"], SampleResource(args.content))
+
+    # Creating the server context
+    asyncio.Task(aiocoap.Context.create_server_context(root_element))
+
+    # This is a message for the parent process to consider this
+    # background subprocess to have successfully run and to return an
+    # exit code of 0
+    print("Process successful\n")
+
+    # Running the server
+    asyncio.get_event_loop().run_forever()
+
+
+if __name__ == "__main__":
+    main()
