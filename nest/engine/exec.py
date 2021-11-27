d3e7b6d9f985429954cb5dace5d02df5d7fd890c
Suhas K S <suhasks123@gmail.com>
2021-11-27 20:47:31 +0530

    engine: Implement exec_subprocess_in_background()
     - exec.py: Add function exec_subprocess_in_background() which
       runs a subprocess in the background for event loop based
       procedures like running servers. This is needed since NeST needs
       to be uninterrupted by such procedures.
    
    Signed-off-by: Suhas K S <suhasks123@gmail.com>

--- nest/engine/exec.py
+++ nest/engine/exec.py
@@ -113,3 +113,63 @@ def exec_exp_commands(cmd, stdout=PIPE, stderr=PIPE, timeout=None):
             proc.kill()
             stderr.write(b"Connection timeout")
         return proc.returncode
+
+
+def exec_subprocess_in_background(cmd, shell=False, wait_for_exit_code=False):
+    """
+    Executes a command in the background. This is mainly for
+    commands or programs that have an event loop running,
+    like server components of networking tools, that need to be
+    run in the background so that NeST is uninterrupted.
+
+    If a custom program is to be run using the command,
+    the `wait_for_exit_code` parameter can be set to true,
+    requiring the custom program to not print anything to
+    stdout except the string 'Process successful\n' when
+    it can be confirmed that the process has run successfully
+    till the point of the event loop. This signifies a success
+    in running the process.
+
+    If `wait_for_exit_code` is set to False, the function always
+    returns 0 as the exit code, indicating success. This is used
+    in situations where the above condition of printing only
+    'Process successful\n' to stdout is not possible.
+
+    Parameters
+    ----------
+    cmd : str
+        command to be executed
+    shell : boolean
+        Spawns a shell and executes the command if true
+        (Default value = False)
+    wait_for_exit_code: boolean
+        To wait or not to wait for a success message from
+        the subprocess
+
+    Returns
+    -------
+    int
+        Return code received after executing the command
+    """
+    # If shell is set to True, use the command as it is
+    temp_cmd = cmd
+
+    # If shell is set to False, split the command
+    if shell is False:
+        temp_cmd = cmd.split()
+
+    # Create and run the subprocess
+    # pylint:disable=consider-using-with
+    proc = Popen(temp_cmd, stdout=PIPE, stderr=PIPE, shell=shell)
+
+    # If wait_for_exit_code is set to True
+    if wait_for_exit_code:
+        # Continuously read from the stdout buffer till newline
+        # is encountered
+        line_in_bytes = proc.stdout.readline()
+        line_str = line_in_bytes.decode("utf-8")
+        if line_str == "Process successful\n":
+            return 0
+        return 1
+
+    return 0
