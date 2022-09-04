56caa105091bd8557b4ca0969b9913a7609a8788
Mohit P. Tahiliani <tahiliani.nitk@gmail.com>
2022-09-04 11:20:46 +0530

    examples: Update address-helpers and README
    Signed-off-by: Mohit P. Tahiliani <tahiliani.nitk@gmail.com>

--- examples/address-helpers/README.md
+++ examples/address-helpers/README.md
@@ -74,7 +74,22 @@ simpler approach to use the `Network` API.
 <!-- The below snippet will render example code in docs website -->
 <!-- #DOCS_INCLUDE: ah-two-lans-connected-directly.py -->
 
-## 6. ipv6-ah-point-to-point-1.py
+## 6. ah-two-lans-connected-via-router.py
+This program emulates two Local Area Networks (LANs) connected via a router.
+LAN-1 consists three hosts `h1` to `h3` connected to switch `s1`, and LAN-2
+consists three hosts `h4` to `h6` connected to switch `s2`. Switches `s1`
+and `s2` are connected to each other via a router `r1`. Five ping packets are
+sent from `h1` to `h4`, five from `h2` to `h5` and lastly, five from `h3` to
+`h6`. The success/failure of these packets is reported. It is similar to
+`two-lans-connected-via-router.py` available in `examples/basic-examples`,
+the only difference is that we use an address helper in this program to
+assign IPv4 addresses to interfaces instead of manually assigning them. Note
+that two packages: `Network` and `AddressHelper` are imported in this program.
+
+<!-- The below snippet will render example code in docs website -->
+<!-- #DOCS_INCLUDE: ah-two-lans-connected-via-router.py -->
+
+## 7. ipv6-ah-point-to-point-1.py
 This program emulates a point to point network between two hosts `h1` and
 `h2`. Five ping packets are sent from `h1` to `h2`, and the success/failure
 of these packets is reported. It is similar to `ipv6-point-to-point-1.py`
@@ -86,7 +101,7 @@ are imported in this program.
 <!-- The below snippet will render example code in docs website -->
 <!-- #DOCS_INCLUDE: ipv6-ah-point-to-point-1.py -->
 
-## 7. ipv6-ah-point-to-point-2.py
+## 8. ipv6-ah-point-to-point-2.py
 This program emulates point to point networks that connect two hosts `h1`
 and `h2` via a router `r1`. Five ping packets are sent from `h1` to `h2`, and
 the success/failure of these packets is reported. This program is similar to
@@ -98,7 +113,7 @@ interfaces instead of manually assigning them. Note that two packages:
 <!-- The below snippet will render example code in docs website -->
 <!-- #DOCS_INCLUDE: ipv6-ah-point-to-point-2.py -->
 
-## 8. ipv6-ah-point-to-point-3.py
+## 9. ipv6-ah-point-to-point-3.py
 This program emulates point to point networks that connect two hosts `h1` and
 `h2` via two routers `r1` and `r2`. Five ping packets are sent from `h1` to
 `h2`, and the success/failure of these packets is reported. It is similar to
@@ -110,7 +125,7 @@ interfaces instead of manually assigning them. Note that two packages:
 <!-- The below snippet will render example code in docs website -->
 <!-- #DOCS_INCLUDE: ipv6-ah-point-to-point-3.py -->
 
-## 9. ipv6-ah-simple-lan.py
+## 10. ipv6-ah-simple-lan.py
 This program emulates a Local Area Network (LAN). Four hosts: `h1` to `h4`
 are connected using a switch `s1`. Five ping packets are sent from `h1` to
 `h2`, and five ping packets from `h3` to `h4`. The success/failure of these
@@ -124,7 +139,7 @@ network, we demonstrate a simpler approach to use the `Network` API.
 <!-- The below snippet will render example code in docs website -->
 <!-- #DOCS_INCLUDE: ipv6-ah-simple-lan.py -->
 
-## 10. ipv6-ah-two-lans-connected-directly.py
+## 11. ipv6-ah-two-lans-connected-directly.py
 This program emulates two Local Area Networks (LANs) connected directly to
 each other. LAN-1 consists three hosts `h1` to `h3` connected to switch `s1`,
 and LAN-2 consists three hosts `h4` to `h6` connected to switch `s2`.
--- examples/address-helpers/ah-two-lans-connected-via-router.py
+++ examples/address-helpers/ah-two-lans-connected-via-router.py
@@ -10,25 +10,27 @@ from nest.topology.address_helper import AddressHelper
 
 # This program emulates two Local Area Networks (LANs) connected via a router.
 # LAN-1 consists three hosts `h1` to `h3` connected to switch `s1`, and LAN-2
-# consists three hosts `h4` to `h6` connected to switch `s2`.Switches `s1`
-# and `s2` are connected to each other via a router 'r1'. Five ping packets are
-# sent from `h1` to `h4`, five from `h2` to `h5` and lastly, five from `h3`to `h6`.
-# The success/failure of these packets is reported. It is similar to
-# `two-lans-connected-via-router.py` available in `examples/basic-examples`, the
-# only difference is that we use an address helper in this program to assign
-# IPv4 addresses to interfaces instead of manually assigning them. Note that two
-# packages: `Network` and `AddressHelper` are imported in this program (Lines 8-9 above).
+# consists three hosts `h4` to `h6` connected to switch `s2`. Switches `s1`
+# and `s2` are connected to each other via a router `r1`. Five ping packets are
+# sent from `h1` to `h4`, five from `h2` to `h5` and lastly, five from `h3` to
+# `h6`. The success/failure of these packets is reported. It is similar to
+# `two-lans-connected-via-router.py` available in `examples/basic-examples`,
+# the only difference is that we use an address helper in this program to
+# assign IPv4 addresses to interfaces instead of manually assigning them. Note
+# that two packages: `Network` and `AddressHelper` are imported in this program
+# (Lines 8-9 above).
 
-###############################################################
-#                       Network Topology                      #
-#           LAN-1                           LAN-2             #
-#   h1 ---------------                  ---------------- h4   #
-#                     \                /                      #
-#   h2 --------------- s1 --- r1 --- s2 ---------------- h5   #
-#                     /                \                      #
-#   h3 ---------------                  ---------------- h6   #
-#                                                             #
-###############################################################
+##################################################################
+#                       Network Topology                         #
+#           LAN-1                           LAN-2                #
+#   h1 ---------------                      --------------- h4   #
+#                     \                    /                     #
+#   h2 --------------- s1 ----- r1 ----- s2 --------------- h5   #
+#                     /                    \                     #
+#   h3 ---------------                      --------------- h6   #
+#   <- 100mbit, 1ms ->  <- 10mbit, 10ms ->  <- 100mbit, 1ms ->   #
+#                                                                #
+##################################################################
 
 # Create six hosts 'h1' to 'h6'
 h1 = Node("h1")
@@ -59,30 +61,34 @@ n2 = Network("192.168.2.0/24")  # network on the right of `r1`
 (eth5, ets2b) = connect(h5, s2, network=n2)
 (eth6, ets2c) = connect(h6, s2, network=n2)
 
-# Connect switches `s1` and `s2` to router 'r1'
-(ets1d, etr1) = connect(s1, r1, network=n1)
-(ets2d, etr2) = connect(s2, r1, network=n2)
+# Connect switches `s1` and `s2` to router `r1`
+(ets1d, etr1a) = connect(s1, r1, network=n1)
+(ets2d, etr1b) = connect(s2, r1, network=n2)
 
 # Assign IPv4 addresses to all the interfaces and switches in the network.
 AddressHelper.assign_addresses()
 
-# Set the link attributes
+# Set the attributes of the links between hosts and switches
 eth1.set_attributes("100mbit", "1ms")
 eth2.set_attributes("100mbit", "1ms")
 eth3.set_attributes("100mbit", "1ms")
 eth4.set_attributes("100mbit", "1ms")
 eth5.set_attributes("100mbit", "1ms")
 eth6.set_attributes("100mbit", "1ms")
-etr1.set_attributes("1000mbit", "10ms")
-etr2.set_attributes("1000mbit", "10ms")
 
-# Set the default routes
-h1.add_route("DEFAULT", eth1, etr1.address)
-h2.add_route("DEFAULT", eth2, etr1.address)
-h3.add_route("DEFAULT", eth3, etr1.address)
-h4.add_route("DEFAULT", eth4, etr2.address)
-h5.add_route("DEFAULT", eth5, etr2.address)
-h6.add_route("DEFAULT", eth6, etr2.address)
+# Set the attributes of the links between `r1` and switches
+etr1a.set_attributes("10mbit", "10ms")
+etr1b.set_attributes("10mbit", "10ms")
+
+# Set the default routes for hosts `h1`, `h2` and `h3` via `etr1a`
+h1.add_route("DEFAULT", eth1, etr1a.address)
+h2.add_route("DEFAULT", eth2, etr1a.address)
+h3.add_route("DEFAULT", eth3, etr1a.address)
+
+# Set the default routes for hosts `h4`, `h5` and `h6` via `etr1b`
+h4.add_route("DEFAULT", eth4, etr1b.address)
+h5.add_route("DEFAULT", eth5, etr1b.address)
+h6.add_route("DEFAULT", eth6, etr1b.address)
 
 # `Ping` from `h1` to `h4`, `h2` to `h5`, and `h3` to `h6`.
 h1.ping(eth4.address)
