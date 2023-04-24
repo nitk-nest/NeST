3686cd27e9fcdea185d86e25fb519690e88623ad
Saurabh <singh.abhinandan1503@gmail.com>
2023-04-24 11:54:49 +0530

    examples: Add example for MTR
    Signed-off-by: Abhinandan Singh <singh.abhinandan1503@gmail.com>
    Signed-off-by: Madhunisha E <madhunishamoorthy@gmail.com>
    Signed-off-by: Saurabh Kumar Singh <somu.222cs029@nitk.edu.in>
    Signed-off-by: Vishwas Yadav <contact.vishwas.yadav@gmail.com>

---
+++ examples/debug-options/README.md
@@ -0,0 +1,17 @@
+# Mytraceroute (MTR) Examples in NeST
+
+This directory contains examples to demonstrate how to use Mytraceroute (MTR) in `NeST`.
+
+`IMPORTANT`
+
+Before running the examples, you will need to install mtr. You can do this by running the following command in your terminal:
+
+```shell
+sudo apt install mtr
+```
+
+## 1. mtr-point-to-point-3.py
+This program emulates point to point networks that connect two hosts `h1` and `h2` via two routers `r1` and `r2`. Mytraceroute functionality is performed Five times from `h1` to `h2`and the result is reported. It is similar to `point-to-point-3.py` available in `examples/basic-examples`, the only difference is that Mytraceroute is performed between nodes instead of ping .
+
+<!-- The below snippet will render example code in docs website -->
+<!-- #DOCS_INCLUDE: mtr-point-to-point-3.py -->
---
+++ examples/debug-options/mtr-point-to-point-3.py
@@ -0,0 +1,74 @@
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) 2019-2023 NITK Surathkal
+
+########################
+# SHOULD BE RUN AS ROOT
+########################
+from nest.topology import *
+
+# This program emulates point to point networks that connect two hosts `h1`
+# and `h2` via two routers `r1` and `r2`. Five packets are sent from `h1`
+# to `h2`, and the result of Mytraceroute is reported.
+
+
+##############################################################################
+#                              Network Topology                              #
+#                                                                            #
+#        5mbit, 5ms -->          5mbit, 5ms -->          5mbit, 5ms -->      #
+# h1 -------------------- r1 -------------------- r2 -------------------- h2 #
+#     <-- 10mbit, 100ms       <-- 10mbit, 100ms       <-- 10mbit, 100ms      #
+#                                                                            #
+##############################################################################
+
+# Create two hosts `h1` and `h2`, and two routers `r1` and `r2`
+h1 = Node("h1")
+h2 = Node("h2")
+r1 = Router("r1")
+r2 = Router("r2")
+
+# Connect `h1` to `r1`, `r1` to `r2`, and then `r2` to `h2`
+# `eth1` and `eth2` are the interfaces at `h1` and `h2`, respectively.
+# `etr1a` is the first interface at `r1` which connects it with `h1`
+# `etr1b` is the second interface at `r1` which connects it with `r2`
+# `etr2a` is the first interface at `r2` which connects it with `r1`
+# `etr2b` is the second interface at `r2` which connects it with `h2`
+(eth1, etr1a) = connect(h1, r1)
+(etr1b, etr2a) = connect(r1, r2)
+(etr2b, eth2) = connect(r2, h2)
+
+# Assign IPv4 addresses to all the interfaces.
+# Assign IPv4 addresses to interfaces in the network which is on the left of
+# `r1`. We assume that the IPv4 address of this network is `192.168.1.0/24`
+eth1.set_address("192.168.1.1/24")
+etr1a.set_address("192.168.1.2/24")
+
+# Assign IPv4 addresses to interfaces in the network which is between the two
+# routers. We assume that the IPv4 address of this network is `192.168.2.0/24`
+etr1b.set_address("192.168.2.1/24")
+etr2a.set_address("192.168.2.2/24")
+
+# Assign IPv4 addresses to interfaces in the network which is on the right of
+# `r2`. We assume that the IPv4 address of this network is `192.168.3.0/24`
+etr2b.set_address("192.168.3.1/24")
+eth2.set_address("192.168.3.2/24")
+
+# Set the link attributes: `h1` --> `r1` --> `r2` --> `h2`
+eth1.set_attributes("5mbit", "5ms")  # from `h1` to `r1`
+etr1b.set_attributes("5mbit", "5ms")  # from `r1` to `r2`
+etr2b.set_attributes("5mbit", "5ms")  # from `r2` to `h2`
+
+# Set the link attributes: `h2` --> `r2` --> `r1` --> `h1`
+eth2.set_attributes("10mbit", "100ms")  # from `h2` to `r2`
+etr2a.set_attributes("10mbit", "100ms")  # from `r2` to `r1`
+etr1a.set_attributes("10mbit", "100ms")  # from `r1` to `h1`
+
+# Set default routes in `h1` and `h2`. Additionally, set default routes in
+# `r1` and `r2` so that the packets that cannot be forwarded based on the
+# entries in their routing table are sent via a default interface.
+h1.add_route("DEFAULT", eth1)
+h2.add_route("DEFAULT", eth2)
+r1.add_route("DEFAULT", etr1b)
+r2.add_route("DEFAULT", etr2a)
+
+# `Mytraceroute` from `h1` to `h2`.
+h1.mtr(eth2.address)
