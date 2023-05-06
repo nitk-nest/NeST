ae2d12b29282db4ce004cb569842cf09d947759c
Nitin Dabi <Nitindabi954@gmail.com>
2023-05-06 16:54:24 +0530

    engine: Update capture_packets API
    Signed-off-by: Nitin Dabi <Nitindabi954@gmail.com>

--- nest/engine/t_shark.py
+++ nest/engine/t_shark.py
@@ -3,8 +3,14 @@
 
 """t_shark API management"""
 
-
+import os
+import logging
 from .exec import exec_subprocess
+
+logger = logging.getLogger(__name__)
+
+# Get the current working directory
+current_dir = os.getcwd()
 
 
 def capture_packets(ns_name, **kwargs):
@@ -19,7 +25,20 @@ def capture_packets(ns_name, **kwargs):
     """
 
     tshark_params = ""
+    packet_capture = ""
     for param, value in kwargs.items():
-        tshark_params += param + " " + value + " "
+        if param == "-w":
+            packet_capture = value
 
-    exec_subprocess(f"ip netns exec {ns_name} tshark {tshark_params} -q")
+        tshark_params += f"{param} {value} "
+
+    # Set the path for the pcap file in the current directory
+    pcap_file = f"{current_dir}/packet_capture/{packet_capture}"
+
+    # Create the directory for the pcap file if it doesn't exist
+    os.makedirs(os.path.dirname(pcap_file), exist_ok=True)
+
+    logger.info("The PCAP file is saved in %s", pcap_file)
+
+    exec_subprocess(f"ip netns exec {ns_name} tshark {tshark_params} -w {pcap_file} -q")
+    exec_subprocess(f"ip netns exec {ns_name} chmod +r {pcap_file}")
